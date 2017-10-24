90c1840db2c57ced4e796cacd55d493cbb92470d
Johnreid97 <JREID212@caledonian.ac.uk>
2017-10-24 06:24:58 +0100

    Resp 1
    Initial Setup

---
+++ vrTesting/Assets/Scripts/CrosshairInteraction.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairInteraction : MonoBehaviour {
+
+   public float loadingTime = 3f;
+
+    public float timer;
+    bool staring = false;
+    Coroutine objectLoading;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+        
+
+    }
+    
+    //public void DeleteCube()
+    //{
+    //    if (staring)
+    //    {
+    //        Destroy(this.gameObject);
+    //        print("Working");
+            
+    //    }
+
+        
+    //}
+    public void PointerEnter()
+    {
+        staring = true;
+        objectLoading = StartCoroutine(LoadingObject());
+            
+    }
+
+    public void PointerExit()
+    {
+       
+        if (objectLoading != null)
+        {
+            
+            StopCoroutine(objectLoading);
+            timer = 0f;
+        }
+    }
+
+
+    public IEnumerator LoadingObject()
+    {
+        timer = 0f;
+
+        while (timer < loadingTime)
+        {
+            timer += Time.deltaTime;
+           
+            yield return null;
+            if (timer > loadingTime )
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        
+    }
+    
+}
---
+++ vrTesting/Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+
+    public bool walking = true;
+    public GameObject ground;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Moving();
+        StopMovement();
+
+    }
+
+
+    public void Moving()
+    {
+        
+        if (walking == true)
+        {
+            transform.position = transform.position + Camera.main.transform.forward * 0.5f * Time.deltaTime;
+        }
+    }
+
+    public void StopMovement()
+    {
+        Ray raycast = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(raycast, out hit))
+        {
+             if (hit.collider.name.Contains("Plane"))
+            {
+                walking = false;
+
+            }
+            else
+            {
+                walking = true;
+            }
+        }
+    }
+}
