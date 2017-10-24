95fb50bce683645bcfb1198fd99e335a6e2a86fc
Johnreid97 <JREID212@caledonian.ac.uk>
2017-10-24 11:04:24 +0100

    Resp 2
    Added Railmovement and Initial Design of Grandmothers house

---
+++ vrTesting/Assets/Scripts/Mover.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public RailPoint railPoints;
+
+    int currentSeg;
+    float Transition;
+    bool isCompleted = false;
+    float speed = 2;
+
+    void Update()
+    {
+        if (!railPoints)
+        {
+            return;
+        }
+
+        if (isCompleted == false)
+        {
+            MoveObject();
+        }
+
+
+
+    }
+
+    void MoveObject()
+    {
+        Transition += Time.deltaTime * 1 / speed;
+
+        if (Transition > 1)
+        {
+            Transition = 0;
+            currentSeg++;
+        }
+        else if (Transition < 0)
+        {
+            Transition = 1;
+            currentSeg--;
+        }
+        transform.position = railPoints.Position(currentSeg, Transition);
+
+        if (transform.position == railPoints.lastNode.position)
+        {
+            isCompleted = true;
+        }
+    }
+}
---
+++ vrTesting/Assets/Scripts/RailPoint.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class RailPoint : MonoBehaviour
+{
+
+
+    public Transform[] nodes;
+    public Transform lastNode;
+
+    public Vector3 p1;
+    public Vector3 p2;
+    Quaternion q1;
+    Quaternion q2;
+
+    // Use this for initialization
+    void Start()
+    {
+        nodes = GetComponentsInChildren<Transform>();
+        lastNode = nodes[nodes.Length - 1];
+
+    }
+
+    public Vector3 Position(int whatLine, float whereOnLine)
+    {
+
+
+        p1 = nodes[whatLine + 1].position;
+        p2 = nodes[whatLine + 2].position;
+        return Vector3.Lerp(p1, p2, whereOnLine);
+
+
+
+
+
+    }
+
+
+
+
+    void OnDrawGizmos()
+    {
+        for (int i = 1; i < nodes.Length - 1; i++)
+        {
+            Handles.DrawDottedLine(nodes[i].position, nodes[i + 1].position, 3.0f);
+        }
+    }
+
+}
+
