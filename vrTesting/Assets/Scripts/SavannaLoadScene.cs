ee380bad62d1e26f22b3e4d2e7a214292b07bedf
Johnreid97 <JREID212@caledonian.ac.uk>
2017-11-03 12:47:17 +0000

    Added savanna voiceover/rainforest scene

---
+++ vrTesting/Assets/Scripts/SavannaLoadScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SavannaLoadScene : MonoBehaviour {
+    float timeUntilNextScene = 30.0f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeUntilNextScene -= Time.deltaTime;
+        if (timeUntilNextScene <= 0)
+        {
+
+            SceneManager.LoadScene("Savannah", LoadSceneMode.Single);
+        }
+    }
+}
