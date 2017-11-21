6618b8b440c1c2af9dcc3d6992a790f006533345
Johnreid97 <JREID212@caledonian.ac.uk>
2017-11-21 06:21:31 +0000

    Revert "Merge branch 'master' of https://github.com/Johnreid97/ClimateChangeGame"
    This reverts commit d0cc9ce9862e8f3e73b94c78362e96668e15ff13, reversing
    changes made to e551c33861e153a552ea13d903cba46799ae9dc1.

---
+++ vrTesting/Assets/Scripts/Mover.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    public RailPoint railPoints;
+
+    int currentSeg;
+    float transition;
+    bool isCompleted = false;
+    float speed = 1;
+
+    void Update()
+    {
+        if (!railPoints)
+        {
+            return;
+        }
+
+        if (isCompleted == false)
+        {
+            MoveObject();
+        }
+    }
+
+    void MoveObject()
+    {
+        
+        transition += Time.deltaTime/speed;
+
+        if (transition > 1)
+        {
+            transition = 0;
+            currentSeg++;
+        }
+        else if (transition < 0)
+        {
+            transition = 1;
+            currentSeg--;
+        }
+
+        //if (currentTime > totalTime)
+        //{
+        //    Transition = 0;
+        //    currentSeg++;
+        //}
+        //else if (currentTime < totalTime)
+        //{
+        //    Transition = 1;
+        //    currentSeg--;
+        //}
+
+        
+        transform.position = railPoints.Position(currentSeg, transition);
+
+        if (railPoints.p1 == railPoints.lastNode)
+        {
+            isCompleted = true;
+            transform.position = railPoints.lastNode;
+        }
+    }
+}
