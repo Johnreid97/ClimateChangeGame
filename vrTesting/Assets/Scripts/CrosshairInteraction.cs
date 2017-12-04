2e917a0bea4cf0bec8ee62d2e947e1e9e0e4f877
Johnreid97 <JREID212@caledonian.ac.uk>
2017-12-04 18:08:03 +0000

    new sounds and triggers added

--- vrTesting/Assets/Scripts/CrosshairInteraction.cs
+++ vrTesting/Assets/Scripts/CrosshairInteraction.cs
@@ -6,13 +6,14 @@ using UnityEngine.SceneManagement;
 public class CrosshairInteraction : MonoBehaviour {
 
    public float loadingTime = 3f;
-
+    bool isPlaying = false;
     public float timer;
     bool staring = false;
     Coroutine objectLoading;
     string tag;
     public Animator animator, animator2, animator3, animator4, animator5;
     public Mover mover;
+    public AudioSource sound2, sound3, sound4, sound5, sound6;
 
     
     // Use this for initialization
@@ -22,7 +23,7 @@ public class CrosshairInteraction : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        
         
         
 
@@ -82,6 +83,26 @@ public class CrosshairInteraction : MonoBehaviour {
                     case "SceneSkip2":
                         SceneManager.LoadScene("Rainforest", LoadSceneMode.Single);
                         break;
+                    case "Sound2":
+                        if(isPlaying = false)
+                        sound2.Play();
+                        break;
+                    case "Sound3":
+                        if (isPlaying = false)
+                            sound3.Play();
+                        break;
+                    case "Sound4":
+                        if (isPlaying = false)
+                            sound4.Play();
+                        break;
+                    case "Sound5":
+                        if (isPlaying = false)
+                            sound5.Play();
+                        break;
+                    case "Sound6":
+                        if (isPlaying = false)
+                            sound6.Play();
+                        break;
                 }
                 
             }
@@ -89,5 +110,63 @@ public class CrosshairInteraction : MonoBehaviour {
 
         
     }
-    
+
+    void CheckForAudio()
+    {
+        switch (tag)
+        {
+            case "Sound2":
+                if(sound2.isPlaying)
+                {
+                    isPlaying = true;
+                }
+                else
+                {
+                    isPlaying = false;
+                }
+                break;
+            case "Sound3":
+                if (sound3.isPlaying)
+                {
+                    isPlaying = true;
+                }
+                else
+                {
+                    isPlaying = false;
+                };
+                break;
+            case "Sound4":
+                if (sound4.isPlaying)
+                {
+                    isPlaying = true;
+                }
+                else
+                {
+                    isPlaying = false;
+                }
+                break;
+            case "Sound5":
+                if (sound5.isPlaying)
+                {
+                    isPlaying = true;
+                }
+                else
+                {
+                    isPlaying = false;
+                }
+                break;
+            case "Sound6":
+                if (sound6.isPlaying)
+                {
+                    isPlaying = true;
+                }
+                else
+                {
+                    isPlaying = false;
+                }
+                break;
+        }
+    }
 }
+
+
